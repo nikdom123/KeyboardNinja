65546dffc69428ad62991ad246f407e9258ecde8
gedenteen <razumow.dmitrij@yandex.ru>
2019-05-20 09:52:24 +0700

    Add comments in src/func.cpp

--- src/func.cpp
+++ src/func.cpp
@@ -26,26 +26,26 @@ void Eng()
     int n = str.size(), mistakes = 0, series = 0, max_series = 0;
     k = 0;
     l = 0;
-    clock_t start;
+    clock_t start; // variable declaration
     double duration;
-    start = clock();
+    start = clock(); // start "timer"
     while (l < n) {
         ch = _getch();
         if (ch == 27) { // if the key Esc is pressed
             break;      // then exit
         }
         if (str[l] == ch) { // if the correct character is entered
-            l++;            // then text promotion
+            l++;            // then text promotion,
             cout << ch;
             series++;
-        } else {
+        } else { // if the wrong key is pressed
             mistakes++;
             if (series > max_series)
                 max_series = series;
             series = 0;
         }
     }
-    if (series > max_series)
+    if (series > max_series) // re-check: if no errors were made
         max_series = series;
     duration = (clock() - start) / (double)CLOCKS_PER_SEC;
     cout << "\n\n number of mistakes: " << mistakes << endl;
@@ -80,26 +80,26 @@ void Rus()
     int n = str.size(), mistakes = 0, series = 0, max_series = 0;
     k = 0;
     l = 0;
-    clock_t start;
+    clock_t start; // variable declaration
     double duration;
-    start = clock();
+    start = clock(); // start "timer"
     while (l < n) {
         ch = _getch();
         if (ch == 27) { // if the key Esc is pressed
             break;      // then exit
         }
         if (str[l] == ch) { // if the correct character is entered
-            l++;            // then text promotion
+            l++;            // then text promotion,
             cout << ch;
             series++;
-        } else {
+        } else { // if the wrong key is pressed
             mistakes++;
             if (series > max_series)
                 max_series = series;
             series = 0;
         }
     }
-    if (series > max_series)
+    if (series > max_series) // re-check: if no errors were made
         max_series = series;
     duration = (clock() - start) / (double)CLOCKS_PER_SEC;
     cout << "\n\n number of mistakes: " << mistakes << endl;
