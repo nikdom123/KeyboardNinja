4aea0c4e31fb66d60f3a186070a2e1a0e8a95ec6
gedenteen <razumow.dmitrij@yandex.ru>
2019-05-30 07:51:21 +0700

    Add tests for function c_time

--- test/main.c
+++ test/main.c
@@ -7,3 +7,39 @@ int main(int argc, const char** argv)
 {
     return ctest_main(argc, argv);
 }
+
+CTEST(time_test1, success)
+{
+    int result = c_time(1, 20, 18.76);
+    ASSERT_EQUAL(0, result);
+}
+CTEST(time_test2, success)
+{
+    int result = c_time(1, 20, 20);
+    ASSERT_EQUAL(0, result);
+}
+CTEST(time_test3, failure)
+{
+    int result = c_time(1, 60, 76.2);
+    ASSERT_EQUAL(1, result);
+}
+CTEST(time_test4, failure)
+{
+    int result = c_time(2, 60, 50.854);
+    ASSERT_EQUAL(1, result);
+}
+CTEST(time_test5, success)
+{
+    int result = c_time(2, 60, 16.48);
+    ASSERT_EQUAL(0, result);
+}
+CTEST(time_test6, failure)
+{
+    int result = c_time(3, 60, 16.48);
+    ASSERT_EQUAL(1, result);
+}
+CTEST(time_test7, success)
+{
+    int result = c_time(3, 60, 5.52);
+    ASSERT_EQUAL(0, result);
+}
