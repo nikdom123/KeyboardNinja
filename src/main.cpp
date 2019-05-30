676903a6110b96f1dd666edacbe3a518b72faf83
nikdom123 <nikdom180200@gmail.com>
2019-05-30 18:11:29 +0700

    Correct code main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,8 @@
 #include "func.h"
 #include "menu.h"
-#include <iostream>
-#include <locale.h>
-#include <stdio.h>
-#include <windows.h>
-using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL, "Rus");
     Menu();
     return 0;
 }
